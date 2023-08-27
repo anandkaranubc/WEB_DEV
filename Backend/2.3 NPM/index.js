16bd7a418a7b73bef5bb5ba26d4b3700cd743b14
Karan Anand <anandkarancompsci@gmail.com>
2023-08-27 01:21:32 -0700

    Node JS started

---
+++ Backend/2.3 NPM/index.js
@@ -0,0 +1,14 @@
+// var generateName = require('sillyname');
+// var sillyName = generateName();
+
+// import generateName from "sillyname";
+
+// console.log('My name is ' + sillyName);
+
+// const superheroes = require('superheroes');
+
+import superheroes from "superheroes";
+
+var name = superheroes.random();
+
+console.log('My name is ' + name);
