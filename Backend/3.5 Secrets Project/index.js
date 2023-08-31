27904279e2e53cc8e08b25a987f2512daf012d5a
Karan Anand <anandkarancompsci@gmail.com>
2023-08-31 12:15:18 -0700

    Middlewares completed

--- Backend/3.5 Secrets Project/index.js
+++ Backend/3.5 Secrets Project/index.js
@@ -1,3 +1,49 @@
 //To see how the final website should work, run "node solution.js".
 //Make sure you have installed all the dependencies with "npm i".
 //The password is ILoveProgramming
+
+
+import bodyParser from "body-parser";
+import express from "express";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+// import bodyParser from "body-parser";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const app = express();
+const port = 3000;
+
+var autho = false;
+
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+function checkPass(req, res, next) {
+    var pass = "ILoveProgramming";
+    if (pass == req.body["password"]) {
+        autho = true;
+    }
+    next();
+}
+
+app.use(checkPass);
+
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/public/index.html");
+  });
+
+app.post("/check", (req, res) => {
+  if (autho) {
+    res.sendFile(__dirname + "/public/secret.html");
+  }
+  else {
+    res.redirect("/");
+  }
+})
+
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
